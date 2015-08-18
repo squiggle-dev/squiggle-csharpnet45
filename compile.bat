d253503db2f1660b3706a5210e3325236d0aaeb7
Andy White <andy@soundintheory.co.uk>
2015-08-18 15:24:04 +0100

    Add libraries

--- compile.bat
+++ compile.bat
@@ -1,3 +1,3 @@
 SET CSCPATH=%SYSTEMROOT%\Microsoft.NET\Framework\v4.0.30319
-%CSCPATH%\csc /reference:bin/Newtonsoft.Json.dll;bin/RestSharp.dll /target:library /out:bin/.dll /recurse:src\*.cs /doc:bin/.xml
+%CSCPATH%\csc /reference:lib/Newtonsoft.Json.dll;lib/RestSharp.dll /target:library /out:bin/.dll /recurse:src\*.cs /doc:bin/.xml
 
